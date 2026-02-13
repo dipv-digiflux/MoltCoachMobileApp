cd26b82fa8c7750939041f6a2a8c2d3a240800c0
Didiflux Mac <didifluxmac@Digifluxs-Mac-mini.local>
2026-02-13 12:39:47 +0530

    fix: fixed the onboarding header issue and few issues

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -3,7 +3,6 @@ import Device from './png/Device.png';
 import HeaderCurvedBg from './png/header-curved-bg.png';
 import LogoWhite from './png/logo-white.png';
 // svgs
-// svgs
 import BackIconSvg from './svg/back-icon.svg';
 import LogoWhiteSvg from './svg/logo-WhiteSvg.svg';
 
